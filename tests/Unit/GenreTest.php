336b400d3790010603dc3582d67fdb0165a5243f
Maëlys Bühler <m.buhler@outlook.com>
2023-10-14 11:59:35 +0200

    add Genre Model

---
+++ tests/Unit/GenreTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Genre;
+use Tests\TestCase;
+
+class GenreTest extends TestCase
+{
+    public function testCreateStoreAndRetrieve(): void
+    {
+        $genreName = "Test Genre";
+        $genre = new Genre;
+        $genre->name = $genreName;
+        $genre->save();
+        $id = $genre->id;
+
+        $genreRetrieved = Genre::find($id);
+
+        $this->assertTrue($genreRetrieved->name == $genreName);
+    }
+}
