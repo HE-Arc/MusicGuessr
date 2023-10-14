e2db95ca2af9d5eda25b6a584b40cbb754b77d8d
Maëlys Bühler <m.buhler@outlook.com>
2023-10-14 11:44:22 +0200

    add User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     *
+     * indicates to Eloquents that there is no timestamps in the table
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 
     /**
-     * The attributes that are mass assignable.
      *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
+     * set default values
      *
-     * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+    protected $attributes = [
+        'nb_music_found' => 0,
+        'music_streak' => 0,
     ];
 }
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testCreateStoreAndRetrieve(): void
+    {
+        $username = "new_user";
+        $user = new User();
+        $user->name = $username;
+        $user->password= "password_hash";
+        $user->save();
+        $id = $user->id;
+
+        $userRetrieved = User::find($id);
+        $user->delete();
+        $this->assertTrue($userRetrieved->name == $username);
+    }
+}
