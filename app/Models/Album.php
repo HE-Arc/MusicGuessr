a79f8b0595a34a9472cdafce5c7d569337288a68
Maëlys Bühler <m.buhler@outlook.com>
2023-10-14 11:51:34 +0200

    add Album Model

---
+++ app/Models/Album.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    /**
+     *
+     * indicates to Eloquents that there is no timestamps in the table
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ tests/Unit/AlbumTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Album;
+use Tests\TestCase;
+
+class AlbumTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function testCreateStoreAndRetrieve(): void
+    {
+        $albumName = "Test Album";
+        $album = new Album;
+        $album->name = $albumName;
+        $album->save();
+        $id = $album->id;
+
+        $albumRetrieved = Album::find($id);
+        $this->assertTrue($albumRetrieved->name == $albumName);
+    }
+}
