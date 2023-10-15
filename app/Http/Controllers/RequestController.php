ea1527f2483cd97f19eb647a8d38eaf10dbf8dc3
Maëlys Bühler <m.buhler@outlook.com>
2023-10-15 15:33:18 +0200

    add RequestController.php

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SongRequest;
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('requests');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        SongRequest::create($request->only(['song_name', 'artist_name']));
+        return redirect()->route('song_requests.create');
+    }
+}
