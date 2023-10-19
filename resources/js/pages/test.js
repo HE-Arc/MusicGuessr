7b4a7e2be90d976a922142064a8ea579bf88dcce
Plumey Simon <simon.plumey@he-arc.ch>
2023-10-19 17:59:45 +0200

    Spliting vuejs call in specific file

---
+++ resources/js/pages/test.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import Test from '../components/test.vue';
+
+const app = createApp({});
+
+app.component('test-component', Test);
+
+app.mount("#app");
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,4 +3,5 @@
 <div id="app">
     <test-component></test-component>
 </div>
+@vite(['resources/js/pages/test.js'])
 @endsection
