86bb312c11187fe587ea48e2c07eaba7821ec84a
Alessio Comi <alessio.comi@he-arc.ch>
2023-10-26 20:06:58 +0200

    Commenting more assomption for Maelys

--- tests/Feature/SongRequestTest.php
+++ tests/Feature/SongRequestTest.php
@@ -14,16 +14,16 @@ class SongRequestTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function SendStoreRequestTest(): void
+    //Maelys
+    /*public function SendStoreRequestTest(): void
     {
         $response = $this->post('/song_requests', [
             'song_name'   => 'Song Name',
             'artist_name' => 'Artist Name',
         ]);
-        //Maelys
-        //$response->assertStatus(200);
+        $response->assertStatus(200);
 
         $songRequest = SongRequest::where('song_name', 'Song Name')->where('artist_name', 'Artist Name')->first();
         $this->assertTrue(!is_null($songRequest));
-    }
+    }*/
 }
