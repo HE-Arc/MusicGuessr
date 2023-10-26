8eccbe798512338710825910ba36d2dc9bf60f1e
Alessio Comi <alessio.comi@he-arc.ch>
2023-10-26 15:55:45 +0200

    Requested song, and confirmation pages made

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -20,10 +20,10 @@ class RequestController extends Controller
      */
     public function store(Request $request)
     {
-        SongRequest::create($request->only(['song_name', 'artist_name']));
+        $songRequest = SongRequest::create($request->only(['song_name', 'artist_name']));
 
-        return view('request.confirmation');
-
-        //return redirect()->route('song_requests.create');
+        return view('request.confirmation')->with(['title' => $songRequest->song_name, 'author' => $songRequest->artist_name]);
     }
+
+
 }
