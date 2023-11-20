7b6289d2255a2c11c2c9065accd27d641f984b63
Maëlys Bühler <m.buhler@outlook.com>
2023-11-20 15:21:06 +0100

    Add has_game_started route

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -11,23 +11,22 @@ class SongController extends Controller
     public function getSongBeginningWith(Request $request, string $searchString): string
     {
         $songs_array = Song::getInfoOnSongBeginningWith($searchString);
-        header('Content-Type: application/json');
-
+        header("Content-Type: application/json");
+        http_response_code(200);
         return json_encode($songs_array);
     }
 
     public function getCommonPointsBetweenSongs(Request $request): string
     {
         if (!$request->session()->exists('answerSong')) {
-            return json_encode(['status' => 'error', 'message' => 'No answer song in session.']);
+            return response(json_encode(['message' => 'No answer song in session, start the game and retry.']), 449)->header('Content-Type', 'application/json');
         }
 
         $answerSong = session('answerSong');
         $song = Song::findOrFail($request->song_id);
         $answer = $song->getComparisonArray($answerSong);
-        $answer['status'] = 'success';
-        header('Content-Type: application/json');
-
+        header("Content-Type: application/json");
+        http_response_code(200);
         return json_encode($answer);
     }
 }
