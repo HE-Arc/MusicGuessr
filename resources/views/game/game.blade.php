c71a00b69cc66dbfe48861852af4641680d97653
Plumey Simon <simon.plumey@he-arc.ch>
2023-11-21 09:45:38 +0100

    Persistant storage

--- resources/views/game/game.blade.php
+++ resources/views/game/game.blade.php
@@ -1,12 +1,13 @@
 @extends('layout.app')
 @section('content')
+    <div class="vue-container" id="app">
+        <p class="instructions">Commence par taper le titre d'une musique pour découvrir des indices:</p>
+        <search-bar @delta-with-answer-song="send-delta-to-comp"></search-bar>
+        <h2>Musique à deviner</h2>
+        <comparaison :></comparaison>
+    </div>
+    <!-- temporary, until navBar -->
+    <a href="/song_requests/create">Proposer une musique</a>
 
-<div class="search-bar-container" id="app">
-    <p class="instructions">Commence par taper le titre d'une musique pour découvrir des indices:</p>
-    <search-bar></search-bar>
-</div>
-<!-- temporary, until navBar -->
-<a href="/song_requests/create">Proposer une musique</a>
-
-@vite(['resources/js/pages/game.js'])
+    @vite(['resources/js/pages/game.js'])
 @endsection
