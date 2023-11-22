930cd73941889a85598738bfdff8979e74bfbde9
Plumey Simon <simon.plumey@he-arc.ch>
2023-11-22 17:57:17 +0100

    remove old link

--- resources/views/game/game.blade.php
+++ resources/views/game/game.blade.php
@@ -6,8 +6,5 @@
         <comparaison :data="deltaToSend"></comparaison>
         <history :data="deltaToSend"></history>
     </div>
-    <!-- temporary, until navBar -->
-    <a href="/song_requests/create">Proposer une musique</a>
-
     @vite(['resources/js/pages/game.js'])
 @endsection
