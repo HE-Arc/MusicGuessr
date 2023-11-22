a85024569191a393a991da47720042a3d3745600
Plumey Simon <simon.plumey@he-arc.ch>
2023-11-22 17:00:10 +0100

    Naviguation

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,3 +1,27 @@
-<nav class="navbar">
-
+<nav class="navbar" id="sideBarNav">
+    <a href="javascript:void(0)" class="closeNavBtn" onclick="closeNav()">&times;</a>
+    <a href="/">Jeu</a>
+    <a href="/song_requests/create">Proposer une musique</a>
+    <a href="/">Connexion</a>
+    <a href="/">Inscription</a>
 </nav>
+
+<span onclick="openNav()" class="openNavBtn">
+    <div class="icon-burger">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</span>
+
+<script>
+    function openNav() {
+        document.getElementById("sideBarNav").style.width = "250px";
+        document.getElementById("body").style.marginRight = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("sideBarNav").style.width = "0";
+        document.getElementById("body").style.marginRight = "0";
+    }
+</script>
