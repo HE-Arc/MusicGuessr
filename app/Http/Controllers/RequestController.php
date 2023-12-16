dea96564aeb37d0832a9c48f9ab7c169478428de
Alessio Comi <alessio.comi@he-arc.ch>
2023-12-16 10:12:25 +0100

    Removing mail because not being able to connect to smtp, adding migration to make user non unique, adding frontend for entry validation of songs request

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -20,8 +20,9 @@ class RequestController extends Controller
      */
     public function store(Request $request)
     {
+        //@ Maelys 
+        //$songRequest = SongRequest::create($request->validated()->only(['song_name', 'artist_name']));
         $songRequest = SongRequest::create($request->only(['song_name', 'artist_name']));
-
         return view('request.confirmation')->with(['title' => $songRequest->song_name, 'author' => $songRequest->artist_name]);
     }
 }
--- resources/views/request/proposal.blade.php
+++ resources/views/request/proposal.blade.php
@@ -8,7 +8,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="/song_requests">
+    <form method="POST" action="/song_requests" id="songRequestForm">
         @csrf
 
         <!-- Song Name -->
@@ -16,6 +16,7 @@
             <x-input-label for="song" :value="__('Titre de la musique')" />
             <x-text-input id="song" class="block mt-1 w-full" type="text" name="song_name" :value="old('song_name')" required autofocus />
             <x-input-error :messages="$errors->get('song_name')" class="mt-2" />
+            <span id="songErrorMessage" class="text-red-500"></span>
         </div>
 
         <!-- Artist Name -->
@@ -23,10 +24,32 @@
             <x-input-label for="artist" :value="__('Nom de l\'artiste')" />
             <x-text-input id="artist" class="block mt-1 w-full" type="text" name="artist_name" :value="old('artist_name')" required />
             <x-input-error :messages="$errors->get('artist_name')" class="mt-2" />
+            <span id="artistErrorMessage" class="text-red-500"></span>
         </div>
 
         <button type="submit" class="mt-4">Proposer</button>
     </form>
 </div>
 
+<script>
+    document.getElementById('songRequestForm').addEventListener('submit', function (event) {
+        var songInput = document.getElementById('song');
+        var artistInput = document.getElementById('artist');
+        var songErrorMessage = document.getElementById('songErrorMessage');
+        var artistErrorMessage = document.getElementById('artistErrorMessage');
+
+        songErrorMessage.innerHTML = '';  // Réinitialise les messages d'erreur
+        artistErrorMessage.innerHTML = '';
+
+        if (songInput.value.length > 25) {
+            songErrorMessage.innerHTML = 'Le titre de la musique ne peut pas dépasser 25 caractères.';
+            event.preventDefault();
+        }
+
+        if (artistInput.value.length > 25) {
+            artistErrorMessage.innerHTML = 'Le nom de l\'artiste ne peut pas dépasser 25 caractères.';
+            event.preventDefault();
+        }
+    });
+</script>
 @endsection
