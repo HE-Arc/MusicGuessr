fb78eaf3ca722f32b2486a71392314e93ff7c5e7
Plumey Simon <simon.plumey@he-arc.ch>
2023-12-16 17:22:04 +0100

    Clean css

--- resources/views/request/confirmation.blade.php
+++ resources/views/request/confirmation.blade.php
@@ -1,17 +1,14 @@
 @extends('layout.app')
 @section('content')
 
-<div class="requested-song-form-container">
+<section class="small-container requested-song-form-container">
     <p>Votre proposition a été enregistrée avec succès !</p>
     <p>Titre : {{ $title }}</p>
     <p>Auteur : {{ $author }}</p>
     <a href="{{ route('song_requests.create') }}">
-        <button class="requested-song-form-container" type="button">Proposer une autre musique</button>
+        <button class="btn btn-magenta" type="button">Proposer une autre musique</button>
     </a>
-
-    <a href="/">Retour au jeu</a>
-
-</div>
+</section>
 
 @vite(['resources/js/pages/game.js'])
 @endsection
